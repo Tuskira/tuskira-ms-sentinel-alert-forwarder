e3e77fd3c7e60ff2a5aa5f203205ddbc94f681df
tuskira-spatwardhan <saurabh@tuskira.ai>
2026-06-15 14:42:32 +0530

    Delete tuskira-sentinel-alert-forwarder directory

--- tuskira-sentinel-alert-forwarder/main.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_client_config" "current" {}
-
-data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
-}
-
-locals {
-  prefix = "tsk-${var.tenant_name}-${var.environment}"
-}
